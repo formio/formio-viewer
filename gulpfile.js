066ca158aa9dd7643bb229c15d7ad6eb6511b1c4
Travis Tidwell <travis@form.io>
2016-11-28 23:40:11 -0600

    Moved the form viewer to https://formview.io

--- gulpfile.js
+++ gulpfile.js
@@ -2,77 +2,47 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var del = require('del');
 var vinylPaths = require('vinyl-paths');
-var _ = require('lodash');
-var runSequence = require('run-sequence');
-
-var cleanTasks = [];
-var buildTasks = [];
-_.each(['view'], function(type) {
-    // Clean the dist folder.
-    cleanTasks.push('clean:' + type);
-    gulp.task('clean:' + type, function() {
-        return gulp.src('dist/' + type).pipe(vinylPaths(del));
-    });
-
-    // Create the minified js and css files.
-    gulp.task('minify:' + type, ['clean:' + type], function() {
-        return gulp.src('src/' + type + '/index.html')
-            .pipe(plugins.useref())
-            .pipe(plugins.if('*.js', plugins.uglify()))
-            .pipe(plugins.if('*.css', plugins.minifyCss()))
-            /*.pipe(plugins.if('*.html', plugins.htmlmin({
-                collapseWhitespace: true,
-                minifyJS: true,
-                minifyCSS: true,
-                processScripts: ['text/template']
-            })))*/
-            .pipe(gulp.dest('dist/' + type));
-    });
-
-    // Copy the fonts.
-    gulp.task('fonts:' + type, function() {
-        return gulp.src('./bower_components/bootstrap/fonts/*.*').pipe(gulp.dest('dist/' + type + '/fonts'));
-    });
-
-    // Copy the formio files.
-    gulp.task('formio:' + type, function() {
-        return gulp.src('./bower_components/ng-formio/dist/formio-full**.*').pipe(gulp.dest('dist/' + type));
-    });
-
-    // Copy the fonts.
-    gulp.task('assets:' + type, function() {
-        return gulp.src('assets/**/*').pipe(gulp.dest('dist/' + type + '/assets'));
-    });
-
-    // Define the build task.
-    buildTasks.push('build:' + type);
-    gulp.task('build:' + type, ['minify:' + type, 'fonts:' + type, 'formio:' + type, 'assets:' + type]);
-});
 
 // Clean the dist folder.
-gulp.task('clean', cleanTasks, function() {
+gulp.task('clean', function() {
     return gulp.src('dist').pipe(vinylPaths(del));
 });
 
-// Build the project.
-gulp.task('build', function(cb) {
-    runSequence(['clean'], buildTasks, cb)
+// Create the minified js and css files.
+gulp.task('minify', ['clean'], function() {
+    return gulp.src('src/index.html')
+        .pipe(plugins.useref())
+        .pipe(plugins.if('*.js', plugins.uglify()))
+        .pipe(plugins.if('*.css', plugins.minifyCss()))
+        .pipe(plugins.if('*.html', plugins.htmlmin({
+            collapseWhitespace: true,
+            minifyJS: true,
+            minifyCSS: true,
+            processScripts: ['text/template']
+        })))
+        .pipe(gulp.dest('dist'));
 });
+
+// Copy the fonts.
+gulp.task('fonts', function() {
+    return gulp.src('./bower_components/bootstrap/fonts/*.*').pipe(gulp.dest('dist/fonts'));
+});
+
+// Copy the formio files.
+gulp.task('formio', function() {
+    return gulp.src('./bower_components/ng-formio/dist/formio-full**.*').pipe(gulp.dest('dist'));
+});
+
+// Copy the fonts.
+gulp.task('assets', function() {
+    return gulp.src('assets/**/*').pipe(gulp.dest('dist/assets'));
+});
+
+// Define the build task.
+gulp.task('build', ['minify', 'fonts', 'formio', 'assets']);
 
 // Deployments.
 var s3 = require("gulp-s3");
-gulp.task('deploy', ['build'], function () {
+gulp.task('deploy', function () {
     return gulp.src('./dist/**/*').pipe(s3(require('./aws.json')));
-});
-
-gulp.task('deploy:test', ['build'], function() {
-    var settings = require('./aws.json');
-    settings.bucket = 'test-form.io';
-    return gulp.src('./dist/**/*').pipe(s3(settings));
-});
-
-gulp.task('deploy:prod', ['build'], function() {
-    var settings = require('./aws.json');
-    settings.bucket = 'form.io';
-    return gulp.src('./dist/**/*').pipe(s3(settings));
 });
