ed3dada2277a8323f9a53f0fb4872275270592c6
Travis Tidwell <travis@form.io>
2018-08-15 00:11:39 -0500

    Updated renderer.

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,7 +1,7 @@
 import DateTimeComponent from 'formiojs/components/datetime/DateTime';
 export default class ViewerDateTime extends DateTimeComponent {
-  offset(date) {
-    // Ensure the displayInTimezone in viewer is never "viewer" for server PDF generation to work correctly...
+  get timezone() {
+    // Ensure the displayInTimezone is never viewer...
     if (
       this.options.pdf &&
       (!this.component.displayInTimezone || (this.component.displayInTimezone === 'viewer'))
@@ -13,6 +13,6 @@ export default class ViewerDateTime extends DateTimeComponent {
         this.component.displayInTimezone = 'gmt';
       }
     }
-    return super.offset(date);
+    return super.timezone;
   }
 }
