7acbefa2fba727cbb2c6535231a21cdc617052f9
Travis Tidwell <travis@form.io>
2018-10-10 10:33:41 -0500

    Updated viewer with latest build.

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            babelrc: true
-          }
+          loader: 'babel-loader'
         }
       }
     ]
