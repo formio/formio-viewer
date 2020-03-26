719517d4dd0b19747bb1a4466194b184c7265086
Travis Tidwell <travis@form.io>
2020-03-26 12:59:14 -0500

    Fixed the renderer to include use methods.

--- src/renderer.js
+++ src/renderer.js
@@ -1,13 +1,5 @@
-// Import all components and alter those for this viewer.
+import { Formio, Components } from 'formiojs/formio.form.js';
 import './components/ViewerCalendar';
-import AllComponents from 'formiojs/components';
 import ViewerDateTime from './components/DateTime';
-AllComponents.datetime = ViewerDateTime;
-
-import Formio from 'formiojs/Formio';
-import Components from 'formiojs/components/Components';
-Components.setComponents(AllComponents);
-Formio.Components = Components;
-export Form from 'formiojs/Form';
-export Utils from 'formiojs/utils';
-export { Components, Formio };
+Components.setComponent('datetime', ViewerDateTime);
+export { Formio, Components };
