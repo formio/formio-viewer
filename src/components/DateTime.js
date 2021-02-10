090d8d1dc03fc088b5e993cd2c51560ca3538e2a
Roman Letsuk <romaletsuk@gmail.com>
2021-02-10 15:59:40 +0300

    set component widget submission timezone to don't lose it after redraw

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -8,4 +8,13 @@ export default class ViewerDateTime extends DateTimeComponent {
       this.component.widget.type = 'viewercalendar';
     }
   }
+
+  performInputMapping(input) {
+    if (input.widget && input.widget.settings) {
+      //set to don't lose it after redraw.
+      this.component.widget.submissionTimezone = this.submissionTimezone;
+      input.widget.settings.submissionTimezone = this.submissionTimezone;
+    }
+    return input;
+  }
 }
