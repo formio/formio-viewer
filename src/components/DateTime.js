711a3f30dbcd301476765fdf9f5e57af24bcdade
yavorovsky <vladislav@form.io>
2021-06-29 10:03:41 +0300

    Get rid of the deprecated widget type

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,14 +1,5 @@
 import DateTimeComponent from 'formiojs/components/datetime/DateTime';
 export default class ViewerDateTime extends DateTimeComponent {
-  constructor(component, options, data) {
-    super(component, options, data);
-
-    // Pass along the pdf option to the calendar widget.
-    if (this.options.pdf) {
-      this.component.widget.type = 'viewercalendar';
-    }
-  }
-
   performInputMapping(input) {
     if (input.widget && input.widget.settings) {
       //set to don't lose it after redraw.
