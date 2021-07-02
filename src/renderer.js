91b11d78ee547f56d40a89ed6913b587e07e06cf
yavorovsky <vladislav@form.io>
2021-07-02 16:18:46 +0300

    Added extended textfield component

--- src/renderer.js
+++ src/renderer.js
@@ -3,5 +3,7 @@ import Flatpickr from 'flatpickr';
 window.flatpickr = window['flatpickr-css'] = Flatpickr;
 import './components/ViewerCalendar';
 import ViewerDateTime from './components/DateTime';
+import ViewerTextField from './components/TextField';
 Formio.Components.setComponent('datetime', ViewerDateTime);
+Formio.Components.setComponent('textfield', ViewerTextField);
 export { Formio };
