363a32f5dce30871372dd8a443ba0d23a283a08c
ICX\Tatsiana.Hashtold <tanya@form.io>
2025-01-10 15:40:56 +0300

    FIO-9537: fixed rendering PDFs in html format

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var del = require('del');
+var replace = require('gulp-replace');
+var bootstrapIconsVersion = require('./node_modules/bootstrap-icons/package.json').version;
 gulp.task('clean', () => del(['dist/*', 'lib/*']));
-gulp.task('html', () => gulp.src('./src/index.html').pipe(plugins.htmlmin({
-  collapseWhitespace: true,
-  minifyCSS: true,
-  minifyJS: true
-})).pipe(gulp.dest('dist')));
+gulp.task('html', () => gulp.src('./src/index.html')
+  .pipe(replace('https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.min.css', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@' + bootstrapIconsVersion +'/font/bootstrap-icons.min.css'))
+  .pipe(plugins.htmlmin({
+    collapseWhitespace: true,
+    minifyCSS: true,
+    minifyJS: true
+  }))
+  .pipe(gulp.dest('dist')));
 gulp.task('assets', () => gulp.src('./src/assets/**/*.*', { encoding: false }).pipe(gulp.dest('dist/assets')));
 gulp.task('flatpickr', () => gulp.src('./node_modules/flatpickr/dist/**/*', { encoding: false }).pipe(gulp.dest('dist/lib/flatpickr')));
 gulp.task('formiojs', () => gulp.src('./node_modules/@formio/js/dist/**/*', { encoding: false }).pipe(gulp.dest('dist/lib/formiojs')));
@@ -26,3 +31,8 @@ gulp.task('build', gulp.parallel(
   'bootswatch',
   'moment-timezone'
 ));
+gulp.task('inlinesource', function () {
+  return gulp.src('./dist/*.html')
+    .pipe(plugins.inlineSource())
+    .pipe(gulp.dest('./dist'));
+});
